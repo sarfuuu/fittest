dd665ce5bdf61b4150a7d35059f7e94c1bf1f439
sarfuuu <sarfarazansari3450@gmail.com>
2024-10-28 00:17:26 +0530

    sarfaraz learn 2

--- lib/main.dart
+++ lib/main.dart
@@ -117,7 +117,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        tooltip: 'Sarfaraz learn 2.',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
